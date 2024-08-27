6e99fd0439096a82871bdbe9bad9c3471d58cc9f
Justin Hollister <hollisterjustin2025@serrahs.com>
2024-08-27 16:19:02 -0700

    safety feature and toast

--- lib/src/holes_feature/hole_modify_file_button.dart
+++ lib/src/holes_feature/hole_modify_file_button.dart
@@ -3,18 +3,39 @@ import 'dart:io';
 import 'package:caden_app2/src/holes_feature/holes_feature_detail_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:toastification/toastification.dart';
 
 class HoleModifyFileButton extends StatelessWidget {
   const HoleModifyFileButton({
     super.key,
   });
 
+  static const double maxXLength = 31.0; //in
+  static const double maxYWidth = 4.5; //in
+
   @override
   Widget build(BuildContext context) {
     var appState = Provider.of<HolesFeatureState>(context,listen: false);
 
     return ElevatedButton(
-      onPressed:  () => appState.editFile(modifyFile(context)), 
+      onPressed:  () {
+        if(!isReadyForEdit(context)){
+          toastification.show(
+	          context: context,
+            type: ToastificationType.success,
+            style: ToastificationStyle.flat,
+            title: const Text("Your parameters are not allowed"),
+            description: const Text(
+                "Try checking that your offset and spacing doesn't exceed workspace boundaries. "),
+            alignment: Alignment.center,
+            autoCloseDuration: const Duration(seconds: 4),
+            primaryColor: const Color(0xff4682b4),
+            boxShadow: lowModeShadow,
+	);
+        } else{
+          appState.editFile(modifyFile(context));
+        }
+      }, 
       child: const Text("Modify"),
     );
   }
@@ -68,5 +89,18 @@ class HoleModifyFileButton extends StatelessWidget {
     fileString =  fileString.replaceFirst(RegExp(r'[Y][0-9]+([.]([0-9]+)?)?'), 'Y${appState.spaceBetweenLength.value}', startIndex);
 
     return fileString;
+  }
+
+  bool isReadyForEdit(BuildContext context){
+    var appState = Provider.of<HolesFeatureState>(context,listen: false);
+    double xOff = appState.lengthOffset.value;
+    double yOff = appState.widthOffset.value;
+    double numHolesLength = appState.numHolesLength.value - 1;
+    double numHolesWidth = appState.numHolesWidth.value - 1;
+    double xSpacing = appState.spaceBetweenLength.value;
+    double ySpacing = appState.spaceBetweenWidth.value;
+
+    return (xOff+numHolesLength*xSpacing <= maxXLength) &&
+      (yOff + numHolesWidth*ySpacing <= maxYWidth);
   }
 }
